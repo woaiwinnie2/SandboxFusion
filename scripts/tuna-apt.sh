3d6d3900f2582d2bddd28ccc9d010a9274cd7ff2
Haochen Zhao <woaiwnnie@gmail.com>
2026-02-23 01:00:34 -0800

    support 24.04 docker base

--- scripts/tuna-apt.sh
+++ scripts/tuna-apt.sh
@@ -22,6 +22,15 @@ deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
 ' > /etc/apt/sources.list
 
+elif [ "$1" = "24.04" ]
+then
+
+    echo $'deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ noble-security main restricted universe multiverse
+' > /etc/apt/sources.list
+
 else
 
 echo "ubuntu version $1 not supported"
